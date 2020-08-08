a13aca5ce7daf578a6993e73c973da33c21d77d4
shinekumar23 <shinekumar23@gmail.com>
2020-08-08 22:38:11 +0530

    Validation of last name

--- userRegistrartion.sh
+++ userRegistrartion.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -x
 read -p "Enter first Name: " firstName
+read -p "Enter Last Name: " lastName
 firstNamePattern="^[A-Z]{1}[a-z]{2,}$"
 
 if [[ $firstName =~ $firstNamePattern ]]
 then 
-					echo valid
+					echo "valid first name"
 else 
-					echo Invalid 
+					echo "Invalid first name"
 fi
+
+if [[ $lastName =~ $firstNamePattern ]]
+then 
+               echo "valid last name"
+else 
+               echo "Invalid last name"
+fi
+
+
+
