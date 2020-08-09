ede3d20896c9910490d629ab5f2b9923af88dcc1
shinekumar23 <shinekumar23@gmail.com>
2020-08-09 19:22:59 +0530

    Validation of emailid

--- userRegistrartion.sh
+++ userRegistrartion.sh
@@ -1,21 +1,28 @@
 #!/bin/bash -x
 read -p "Enter first Name: " firstName
 read -p "Enter Last Name: " lastName
+read -p "Enter email id: " emailid
 firstNamePattern="^[A-Z]{1}[a-z]{2,}$"
+emailPattern="^([a-zA-Z]{1}[a-zA-Z0-9]*)([-.+_]{1}[a-z0-9]+)*@([a-z0-9]+)\.([a-z]{2,5})([.]{1}[a-z]{2})?$"
 
 if [[ $firstName =~ $firstNamePattern ]]
 then 
-					echo "valid first name"
+		      echo "valid first name"
 else 
-					echo "Invalid first name"
+		      echo "Invalid first name"
 fi
 
 if [[ $lastName =~ $firstNamePattern ]]
 then 
-               echo "valid last name"
+        	      echo "valid last name"
 else 
-               echo "Invalid last name"
+               	      echo "Invalid last name"
 fi
 
-
+if [[ $emailid =~ $emailPattern ]]
+then 
+		      echo "Valid email address"
+else
+		      echo "Invalid email address"
+fi
 
